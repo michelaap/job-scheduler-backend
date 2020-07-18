6fee12bf01a86f2c1c8ba738b843892fbb027258
Michel A. A. Pereira <michel.augusto@pg.inatel.br>
2020-07-18 10:19:13 -0300

    check date in scheduling

--- src/routes/scheduling.routes.ts
+++ src/routes/scheduling.routes.ts
@@ -1,17 +1,34 @@
 import { Router } from 'express';
 import { uuid } from 'uuidv4';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
 
 const schedulingRouter = Router();
 
-const scheduler = [];
+interface Scheduling {
+  id: string;
+  provider: string;
+  date: Date;
+}
+
+const scheduler: Scheduling[] = [];
 
 schedulingRouter.post('/', (req, res) => {
   const { provider, date } = req.body;
 
+  const parsedDate = startOfHour(parseISO(date));
+
+  const findSchedulingOnTheSameDate = scheduler.find(s =>
+    isEqual(parsedDate, s.date),
+  );
+
+  if (findSchedulingOnTheSameDate) {
+    return res.status(400).json({ message: 'This time already scheduled' });
+  }
+
   const scheduling = {
     id: uuid(),
     provider,
-    date,
+    date: parsedDate,
   };
 
   scheduler.push(scheduling);

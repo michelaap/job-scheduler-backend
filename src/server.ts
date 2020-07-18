51c10d6c4ee27bcd5410c23b3ce260c2ed0e3408
Michel A. A. Pereira <michel.augusto@pg.inatel.br>
2020-07-18 08:46:29 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import routes from './routes';
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(4000, () => {
+  console.log('Server up 🚀');
+});
